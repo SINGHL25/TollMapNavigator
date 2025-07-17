b8190896fccb1bad2afbb5e0cf38c51c209a7e46
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-17 23:06:08 +1000

    Update file_loader.py

--- app/file_loader.py
+++ app/file_loader.py
@@ -1,1 +1,7 @@
+import pandas as pd
+import os
+
+def load_site_data(filename="toll_sites.csv"):
+    path = os.path.join(os.path.dirname(__file__), "..", "data", filename)
+    return pd.read_csv(path)
 
