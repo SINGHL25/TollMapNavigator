854e0071c3c2f9a6b8b40bb9d0fcfb884211436e
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-17 23:06:24 +1000

    Update map_utils.py

--- app/map_utils.py
+++ app/map_utils.py
@@ -1,1 +1,23 @@
+import folium
+from folium.plugins import MarkerCluster
+
+def create_map(df):
+    avg_lat = df["Latitude"].mean()
+    avg_lon = df["Longitude"].mean()
+    
+    m = folium.Map(location=[avg_lat, avg_lon], zoom_start=11)
+
+    cluster = MarkerCluster().add_to(m)
+
+    for _, row in df.iterrows():
+        tooltip = f"{row['Name']} ({row['Type']})"
+        popup = f"{row['Landmark']}<br>{row['Direction']}"
+        folium.Marker(
+            [row["Latitude"], row["Longitude"]],
+            tooltip=tooltip,
+            popup=popup,
+            icon=folium.Icon(color="blue", icon="road", prefix="fa")
+        ).add_to(cluster)
+
+    return m
 
